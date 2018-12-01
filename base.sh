228f43533c8b61993d52094ef77295eb5d76100c
GregThomas88 <thomasgregoryw@gmail.com>
2018-12-01 00:02:39 +0000

    mariaHHHHDB

--- base.sh
+++ base.sh
@@ -10,11 +10,11 @@ pip install lz4 # takes a bit to get 1-3 mins
 pip install scales # takes a bit to get 1-3 mins
 sudo apt-get install libev4 libev-dev # takes 3-4 mins
 sudo apt-get install -y gcc python-dev # Needs a yes from user
-cd ~/python-driver/
+cd python-driver/
 sudo python setup.py install --no-cython # takes 5-10 mins to get
 pip install cassandra-driver # takes 5-10 mins to get
 sudo python setup.py build # takes 1-3 mins
 sudo python setup.py install # takes 1-3 mins
-cd ~
+cd ..
 python -c 'import cassandra; print cassandra.__version__' # make sure everyting was done correctly
 kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="ExternalIP")].address}' >> NodeExIP.txt
--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import cassandra.cluster import Cluster
-  
+from cassandra.policies import DCAwareRoundRobinPolicy
+
 def main():
-    cluster = Cluster()
     NodeIPFile = open("NodeExIP.txt")
     NodeIP = str(NodeIPFile.read())
     NodeIP = NodeIP.split()
-    cluster = Cluster([NodeIP[0], NodeIP[1], NodeIP[2], load_balancing_policy=DCAwareRoundRobinPolicy, port=2880)
+    cluster = Cluster([NodeIP[0], NodeIP[1], NodeIP[2], load_balancing_policy=DCAwareRoundRobinPolicy, port=80)
+    keyspace = "CREATE KEYSPACE IF NOT EXISTS minibank WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor' : 3}"
+    session = cluster.connect(keyspace)
+    #This should be useless stuff
+    #session.set_keyspace('users')
+    #session.execute('USE users')
+    
+    
+    #UUID takes a uuid.UUID datatype
+    #Username, username, password takes a string
+    #subscription, content takes a list
+    session.execute('CREATE TABLE Users (userID UUID PRIMARY KEY, username text, email text, password text, content list, subscription list)')
+    session.execute('INSERT INTO Users (userID, username, email, password, content, subscription)
+                     VALUES(%s, %s, %s, %s, %s, %s)',
+                     (uuid.uuid1(), username, email, password, contentL, subscriptionL)
+                     )
+    rows = session.execute("SELECT userID, username, email from users")
+    for row in rows:
+        print row[0], row[1], row[2]
+
+
 main()
